3b91dc6fd6354d122c08509db99ae66e7c8ddd84
yushi265 <ae265.1108@gmail.com>
2021-05-05 10:00:30 +0900

    デバッガー導入

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -10,12 +10,16 @@ class SellController extends Controller
 {
     public function showSellForm()
     {
-        $categories = PrimaryCategory::with('secondaryCategories')->orderBy('sort_no')->get();
+        $categories = PrimaryCategory::with([
+            'secondaryCategories' => function($query) {
+                $query->orderBy('sort_no');
+            }
+            ])->orderBy('sort_no')->get();
         $conditions = ItemCondition::orderBy('sort_no')->get();
 
         return view('sell', [
             'conditions' => $conditions,
-            'categories' => $categories]
-        );
+            'categories' => $categories
+        ]);
     }
 }
